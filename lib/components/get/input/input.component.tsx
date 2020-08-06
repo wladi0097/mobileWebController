a9af5cb01d1ecdd8098829d6631c6c1c4cc5280c
Wladislaw Koschemako <w.koschemako@findologic.com>
2020-08-06 16:55:03 +0200

    Add basic text validation

--- lib/components/get/input/input.component.tsx
+++ lib/components/get/input/input.component.tsx
@@ -1,5 +1,6 @@
 import {Component, ComponentChild, RenderableProps, VNode} from 'preact';
 import {TextHelper} from '../../../helpers/text.helper';
+import style from './input.component.scss';
 
 interface IProps {
     text?: string;
@@ -9,23 +10,33 @@ interface IProps {
 
 interface IState {
     inputText: string;
+    valid: boolean;
 }
 
 class InputComponent$ extends Component<IProps> {
-    public state: Readonly<IState> = {inputText: ''};
+    public state: Readonly<IState> = {inputText: '', valid: true};
 
     public done(): void {
-        this.props.resolver(this.state.inputText);
+        if (this.validate()) {
+            this.props.resolver(this.state.inputText);
+        } else {
+            this.setState({valid: false});
+        }
     }
 
     public onInput(ev: { target: { value: string; }; }): void {
-        this.setState({inputText: ev.target.value});
+        this.setState({inputText: ev.target.value, valid: true});
+    }
+
+    public validate(): boolean {
+        const input = this.state.inputText;
+        return input.length >= 1;
     }
 
     public render(props?: RenderableProps<IProps>): ComponentChild {
         return <div>
             <h1 style={TextHelper.fontSize(props.text, 0.7)}>{props.text}</h1>
-            <input onInput={this.onInput.bind(this)}/>
+            <input class={this.state.valid ? '' : style.invalid} onInput={this.onInput.bind(this)}/>
             <button onClick={() => this.done()}>{props.buttonText}</button>
         </div>;
     }
